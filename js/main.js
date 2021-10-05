1c509a1877581b497d7db3f91fd2700f83159d67
hulluahven <hulluahven@gmail.com>
2021-10-05 10:47:59 +0300

    исправления по функциям

--- js/main.js
+++ js/main.js
@@ -1,42 +1,28 @@
+// источник для примера: (https://learn.javascript.ru/number#sluchaynoe-tseloe-chislo-ot-min-do-max)
 const getRandomIntegerFrom = function(min, max) {
-  let random = min + Math.random() * (max + 1 - min);
-  if (min < 0) {
+  const random = min + Math.random() * (max + 1 - min);
+  if (min < 0 || min >= max) {
     return false;
   }
 
-  if (min >= max) {
-    alert('параметры указаны неверно!');
-  }
+  return Math.floor(random);
 
-  else {
-    return Math.floor(random);
-  }
-
-}
+};
 
 // случайное целое число из переданного диапазона включительно
 
 getRandomIntegerFrom(0, 10);
 
 const getRandomFloat = function(min, max, amountOfNumbers) {
-  let random = min + Math.random() * (max + 1 - min);
-  if (min < 0) {
+  const random = min + Math.random() * (max + 1 - min);
+  if (min < 0 || min >= max) {
     return false;
   }
 
-  if (min >= max) {
-    alert('параметры указаны неверно!');
-  }
+  return random.toFixed(amountOfNumbers);
 
-  else {
-    return random.toFixed(amountOfNumbers);
-  }
-
-}
+};
 
 // случайное число с плавающей точкой из переданного диапазона включительно
 
 getRandomFloat(0, 10, 4);
-
-// источник для примера: (https://learn.javascript.ru/number#sluchaynoe-tseloe-chislo-ot-min-do-max)
-
