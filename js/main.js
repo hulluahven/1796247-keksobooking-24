4ff9a3369a88ac95021727dd47a6433e2f86c135
hulluahven <hulluahven@gmail.com>
2021-10-15 11:34:42 +0300

    исправления по модулям

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-import {createAnnouncement} from './data.js';
-import {ANNOUNCEMENT_COUNT} from '.constants.js';
+import {createAnnouncement, ANNOUNCEMENT_COUNT} from './data.js';
 
 const announcementArray = Array.from({length:ANNOUNCEMENT_COUNT}, createAnnouncement);
-announcementArray;
+console.log(announcementArray);
 
